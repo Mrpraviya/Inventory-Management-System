22a07d0042f852f416475618a53bfa03c3c7cedc
Yousof <sandeeptha2001praveen@gmail.com>
2025-06-13 16:17:25 +0530

    User Registration and Login

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <div className='text-3xl font-bold underline text-red-500'>
+      React App
+    </div>
+     
+  )
+}
+
+export default App
