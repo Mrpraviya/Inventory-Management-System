988db2b8bfeb53e3501ffcd195cc69ee6b3677db
Yousof <sandeeptha2001praveen@gmail.com>
2025-06-20 01:21:31 +0530

    Admin dashboard sidebar built

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,47 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Root from "./utils/Root";
 import Login from "./pages/Login";
 import ProtecedRoutes from "./utils/ProtectedRoutes";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Root />} />
-        <Route path="/admin/dashboard" element={<h1>Admin dashboard</h1>} />
-        <Route path="/customer/dashboard" element={<ProtecedRoutes requireRole= {["admin"]}>
-          <h1>Customer dashboard</h1>
-        </ProtecedRoutes> }/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/unauthorized" element= {<p className='font-bold text-3xl mt-20 ml-20'>Unauthorized </p>}  ></Route>
+        <Route
+          path="/admin-dashboard"
+          element={
+            <ProtecedRoutes requireRole={["admin"]}>
+              <Dashboard />
+            </ProtecedRoutes>
+          }
+        >
+          <Route index element={<h1>Summary of dashboard</h1>} />
 
+          <Route path="categories" element={<h1>Categories</h1>} />
+
+          <Route path="products" element={<h1>Products</h1>} />
+
+          <Route path="suppliers" element={<h1>Suppliers</h1>} />
+
+          <Route path="orders" element={<h1>Orders</h1>} />
+
+          <Route path="users" element={<h1>Users</h1>} />
+
+          <Route path="profile" element={<h1>Profile</h1>} />
+        </Route>
+        <Route
+          path="/customer/dashboard"
+          element={<h1>Customer dashboard</h1>}
+        />
+
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/unauthorized"
+          element={
+            <p className="font-bold text-3xl mt-20 ml-20">Unauthorized </p>
+          }
+        ></Route>
       </Routes>
     </Router>
   );
